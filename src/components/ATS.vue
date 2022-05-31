e59f40076287013561bf69ab5a460647ce215f91
Chinyass <bodaral17r85@gmail.com>
2022-06-01 00:36:04 +0700

    first commit

---
+++ src/components/ATS.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id='ats'>
+        <h5>{{name}}</h5>
+        <div id='olts'>
+            <p v-for="olt in olts" :key="olt.ip">{{olt.ip}} {{olt.model}} {{olt.firmware}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ats',
+    props: {
+        olts: [],
+        name: String
+    }
+}
+</script>
+<style scoped>
+    #ats{
+        
+        border-radius: 10%;
+        background-color: aquamarine;
+
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,128 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <b-container>
+        <b-row align-v="stretch">
+          <b-col v-for="data in atss" :key="data.name">
+            <ATS :name="data.name" :olts="data.olts" />
+          </b-col>
+        </b-row>
+      </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ATS from '../components/ATS.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    ATS
+  },
+  data: function() {
+    return {
+      atss:[
+        {
+          name:'ATC-3',
+          location: 'центр',
+          address: 'ул.Дружбы 156',
+          olts: [
+            {
+              ip:'10.3.0.26',
+              model: 'LTP 8x',
+              firmware: '4.2.1'
+            }
+          ]
+        },
+        {
+          name:'ATC-6',
+          location: 'башня',
+          address: 'ул.кочетова 156',
+          olts: [
+            {
+              ip:'10.6.0.3',
+              model: 'LTP 4x',
+              firmware: '4.2.3'
+            }
+          ]
+        },
+        {
+          name:'ATC-2',
+          location: 'башня',
+          address: 'ул.кочетова 156',
+          olts: [
+            {
+              ip:'10.6.0.3',
+              model: 'LTP 4x',
+              firmware: '4.2.3'
+            }
+          ]
+        },
+        {
+          name:'ATC-66',
+          location: 'башня',
+          address: 'ул.кочетова 156',
+          olts: [
+            {
+              ip:'10.6.0.3',
+              model: 'LTP 4x',
+              firmware: '4.2.3'
+            }
+          ]
+        },
+        {
+          name:'ATC-9',
+          location: 'башня',
+          address: 'ул.кочетова 156',
+          olts: [
+            {
+              ip:'10.6.0.3',
+              model: 'LTP 4x',
+              firmware: '4.2.3'
+            }
+          ]
+        },
+        {
+          name:'ATC-10',
+          location: 'башня',
+          address: 'ул.кочетова 156',
+          olts: [
+            {
+              ip:'10.6.0.3',
+              model: 'LTP 4x',
+              firmware: '4.2.3'
+            }
+          ]
+        },
+        {
+          name:'ATC-11',
+          location: 'башня',
+          address: 'ул.кочетова 156',
+          olts: [
+            {
+              ip:'10.6.0.3',
+              model: 'LTP 4x',
+              firmware: '4.2.3'
+            }
+          ]
+        },
+        {
+          name:'ATC-12',
+          location: 'башня',
+          address: 'ул.кочетова 156',
+          olts: [
+            {
+              ip:'10.6.0.3',
+              model: 'LTP 4x',
+              firmware: '4.2.3'
+            }
+          ]
+        }
+      ]
+    }
+  },
+
 }
 </script>
+<style scoped>
+  
+</style>
